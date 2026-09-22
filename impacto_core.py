36c663262b8d888e91fd689b236d283875705721
César Robusti <108082162+cesarrobusti@users.noreply.github.com>
2026-09-22 20:30:37 -0300

    Update tokenizer loading to use BertTokenizerFast

--- impacto_core.py
+++ impacto_core.py
@@ -67,8 +67,8 @@ def carregar_referencia():
 
 
 def carregar_modelo():
-    """Carrega o modelo BERT diretamente do Hugging Face (ou pasta local)."""
-    tokenizer = AutoTokenizer.from_pretrained(DIR_MODELO, use_fast=False)
+    """Carrega o tokenizer e o modelo BERT a partir do Hugging Face."""
+    tokenizer = BertTokenizerFast.from_pretrained(DIR_MODELO)
     model = AutoModelForSequenceClassification.from_pretrained(DIR_MODELO)
     model.to(DEVICE)
     model.eval()
